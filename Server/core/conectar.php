d0c06f88bdb2846df9794cb874160a55950d8653
Hugo <hugo.rock20@hotmail.com>
2018-04-23 23:04:12 -0500

    conectar

--- Server/core/conectar.php
+++ Server/core/conectar.php
@@ -16,8 +16,22 @@ class Conectar{
 
     public function conexion(){
         if ($this->driver == "mysql" || $this->driver == null) {
-            # code...
+            $con = new mysqli($this->host, $this->user, $this->pass, $this->database);
+            $con->query("SET NAMES '".$this->charset."'");
         }
+
+        return $con;
+    }
+
+    public function startFluent(){
+        require_once "FluentPDO/FluentPDO.php";
+
+        if($this->driver == "mysql" || $this->driver == null){
+            $pdo = new PDO($this->driver.":dbname=".$this->database, $this->user, $this->pass);
+            $fpdo = new FluentPDO($pdo);
+        }
+
+        return $fpdo;
     }
 }
 
