2eb60f60f199cd69c0d722172da878ad2f7ecc1f
Hugo Roca <hugo.rock20@hotmail.com>
2018-05-18 12:53:40 -0500

    Diseño terminado de pacientes.html

--- App/private/pacientes/pacientes.controller.js
+++ App/private/pacientes/pacientes.controller.js
@@ -8,18 +8,30 @@
     pacientesController.$inject = ['configService', 'authenticationService', 'dataService', 'toastr', '$state'];
     function pacientesController(configService, authenticationService, dataService, toastr, $state) {
         var vm = this;
-        
+        vm.Titulo = '';
 
         vm.mostrarModalCRUD = mostrarModalCRUD;
+        vm.NuevoRegistro = NuevoRegistro;
+        vm.volverLista = volverLista;
 
         activate();
 
         ////////////////
 
-        function activate() { toastr.success('Probando','Mensaje'); }
+        function activate() { vm.Titulo = 'Lista de Pacientes'; toastr.success('Probando','Mensaje'); }
 
         function mostrarModalCRUD() {
             MostrarModal('RegistraEditaModal');
+        }
+        
+        function NuevoRegistro() {
+            $('#Pacientes a[data-target="#RegistroPacientes"]').tab('show');
+            vm.Titulo = 'Registro de Pacientes';            
+        }
+
+        function volverLista(){
+            $('#Pacientes a[data-target="#ListaPacientes"]').tab('show');
+            vm.Titulo = 'Lista de Pacientes'; 
         }
     }
 })();
